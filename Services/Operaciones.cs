1bdc87139fec56dfc40066e70ed5ffdb6cc61074
juxo321 <juxo321@gmail.com>
2022-02-24 22:15:24 +0000

    soap-test docker

---
+++ Services/Operaciones.cs
@@ -0,0 +1,18 @@
+using System;
+using WSDL.mensajes;
+
+namespace WSDL.operaciones
+{
+    public class Operaciones : Mensajes
+    {
+        public string Saludar(string nombre)
+        {
+            string msj = "Hola "+ nombre+ "te saluda Justin";
+            return msj;
+        }
+        public string Mostrar(int id)
+        {
+            return "x";
+        }
+    }
+}
